5dc69c9284cd3fe1ad8e9e14675ff8e5203f171a
Eric Stern <eric@slant.co>
2019-05-17 12:46:39 -0700

    If there's a number in the input text, treat it as number of choices to roll and render them all

--- Sources/App/Controllers/LunchController.swift
+++ Sources/App/Controllers/LunchController.swift
@@ -5,15 +5,33 @@ struct LunchController {
     let restaurants: [Restaurant]
     func pickLunch(_ req: Request) throws -> Future<SlackResponse> {
         return try req.content.decode(SlackRequest.self).map { slackRequest in
-            guard let pick = (self.restaurants
-                .filter { r in r.isOpen }
-                .shuffled()
-                .first
-            ) else {
+            var options = self.restaurants.filter { r in r.isOpen }
+            guard options.count > 0 else {
                 return SlackResponse(text: "Nothing is open!?", attachments: [])
             }
 
-            return SlackResponse(text: "\(pick.name) (\(pick.cuisine))", attachments: [])
+            var pickCount = 1
+
+            for word in slackRequest.text.split(separator: " ") {
+                if let intVal = Int(word) {
+                    pickCount = intVal
+                    break
+                }
+            }
+
+            var picks = [Restaurant]()
+            while pickCount > 0 && options.count > 0 {
+                let pick = options.shuffled().first!
+                picks.append(pick)
+                options = options.filter { r in r.cuisine != pick.cuisine }
+                pickCount -= 1
+            }
+
+            let responseText = picks.map { pick in
+                "\(pick.name) (\(pick.cuisine))"
+            }.joined(separator: "\n")
+
+            return SlackResponse(text: responseText, attachments: [])
         }
     }
 }
